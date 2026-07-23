23f2e5902deaa128867fea867d72f83171cf82fc
María José <mjrr39sevilla@gmail.com>
2026-07-23 08:58:04 +0200

    Move API folder to project root for Vercel

--- api/football-proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    const{path} = req.query;
-    const url = `https://api.football-data.org/v4/${path}`;
-    const response = await fetch(url, {
-        headers: { 'X-Auth-Token': process.env.FOOTBALL_API_KEY }
-    });
-    const data = await response.json();
-    res.status(response.status).json(data);
-    }
